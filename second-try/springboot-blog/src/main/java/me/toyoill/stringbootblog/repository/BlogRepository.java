04e2f8f500262ec983e0a543078a53d33bd95a1b
N3wCP <61874428+Toyoill@users.noreply.github.com>
2023-08-11 12:14:54 +0900

    Change directory

--- second-try/springboot-blog/src/main/java/me/toyoill/stringbootblog/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.toyoill.stringbootblog.repository;
-
-import me.toyoill.stringbootblog.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
