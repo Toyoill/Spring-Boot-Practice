f9b40a06c5149453a9aba6442c1c473419897722
N3wCP <61874428+Toyoill@users.noreply.github.com>
2023-08-11 12:33:58 +0900

    Revert "Change directory"
    This reverts commit 04e2f8f500262ec983e0a543078a53d33bd95a1b.

---
+++ first-try/springboot-blog/src/main/java/me/toyoill/springbootblog/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package me.toyoill.springbootblog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
---
+++ second-try/springboot-blog/src/main/java/me/toyoill/stringbootblog/dto/AddArticleRequest.java
@@ -0,0 +1,22 @@
+package me.toyoill.stringbootblog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import me.toyoill.stringbootblog.domain.Article;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+    private String title;
+    private String content;
+
+    public Article toEntity(){
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
