30c8a366fd8d31361ff731d5320848c9f2e97159
N3wCP <61874428+Toyoill@users.noreply.github.com>
2023-08-11 17:31:44 +0900

    Article View 추가

--- self-try/springboot-blog/src/main/java/me/toyoill/stringbootblog/controller/BlogViewController.java
+++ self-try/springboot-blog/src/main/java/me/toyoill/stringbootblog/controller/BlogViewController.java
@@ -2,10 +2,12 @@ package me.toyoill.stringbootblog.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.toyoill.stringbootblog.dto.ArticleListViewResponse;
+import me.toyoill.stringbootblog.dto.ArticleViewResponse;
 import me.toyoill.stringbootblog.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,5 +27,14 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        ArticleViewResponse article = new ArticleViewResponse(blogService.findById(id));
+
+        model.addAttribute("article", article);
+
+        return "article";
     }
 }
