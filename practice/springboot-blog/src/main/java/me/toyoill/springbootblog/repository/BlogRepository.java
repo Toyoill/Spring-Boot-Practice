5905b9db3a5af01a73ba61358c076d848031988c
N3wCP <61874428+Toyoill@users.noreply.github.com>
2023-08-11 12:33:44 +0900

    clear

--- practice/springboot-blog/src/main/java/me/toyoill/springbootblog/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.toyoill.springbootblog.repository;
-
-import me.toyoill.springbootblog.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
